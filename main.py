0a8966b45dcda2384133cba07f1f96efe138de6f
Spagio97 <hmckessner@gmail.com>
2026-01-02 22:34:51 +1100

    Ch3 L1

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from stats import get_num_words, get_char_appearances
+from stats import get_num_words, get_char_appearances, sorted_dicts
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
+    char_dict = get_char_appearances(book_text)
+    sorted_dict = sorted_dicts(char_dict)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")    
     print("Found " + str(get_num_words(book_text)) + " total words")
-    print (get_char_appearances(book_text))
+    print("--------- Character Count -------")
+    for dict in sorted_dict:
+        if dict["char"].isalpha() == True:
+            print (dict["char"] + ": " + str(dict["num"]))
+    print("============= END ===============")
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
--- stats.py
+++ stats.py
@@ -9,3 +9,16 @@ def get_char_appearances(book_text):
     for char in char_set:
         char_dict[char] = lower_book_text.count(char)
     return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sorted_dicts(dict):
+    dict_list = []
+    for char in dict: 
+        dict_list.append({
+            "char": char,
+            "num": dict[char]
+        })
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
