315dd30bc1205b606cfdce9cb2f19af8c199cc5d
Spagio97 <hmckessner@gmail.com>
2026-01-02 11:09:22 +1100

    Ch2 L4

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print("Found " + str(num_words(book_text)) + " total words")
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
-def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    print(book_text)
+def num_words(book_text):
+    words_list = book_text.split()
+    return len(words_list)
 
 main()
